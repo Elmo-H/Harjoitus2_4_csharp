ccdf74c224108696f07d42175aae9fb3ad07554a
Elmo-H <elmo.huoku@gmail.com>
2022-04-04 20:21:21 +0300

    Got radiobuttons working.

--- Harjoitus2_4_csharp/MainPage.xaml.cs
+++ Harjoitus2_4_csharp/MainPage.xaml.cs
@@ -30,21 +30,30 @@ namespace Harjoitus2_4_csharp
         private void radioButton1_Checked(object sender, RoutedEventArgs e)
         {
             RadioButton radioButton = sender as RadioButton;
+            string value = radioButton.Content.ToString();
+            result.Text = value;
         }
 
         private void radioButton2_Checked(object sender, RoutedEventArgs e)
         {
             RadioButton radioButton = sender as RadioButton;
+            string value = radioButton.Content.ToString();
+            result.Text = value;
         }
 
         private void radioButton3_Checked(object sender, RoutedEventArgs e)
         {
             RadioButton radioButton = sender as RadioButton;
+            string value = radioButton.Content.ToString();
+            result.Text = value;
         }
 
         private void checkBox1_Checked(object sender, RoutedEventArgs e)
         {
-           
+           if (checkBox1.IsChecked == true)
+            {
+                string checkBoxContent = checkBox1.Text;
+            }
         }
 
         private void checkBox2_Checked(object sender, RoutedEventArgs e)
@@ -60,21 +69,19 @@ namespace Harjoitus2_4_csharp
         private void radioButton1_Unchecked(object sender, RoutedEventArgs e)
         {
             RadioButton radioButton = sender as RadioButton;
-        }
-
-        private void radioButton1_Unchecked_1(object sender, RoutedEventArgs e)
-        {
-            RadioButton radioButton = sender as RadioButton;
+            result.Text = "";
         }
 
         private void radioButton2_Unchecked(object sender, RoutedEventArgs e)
         {
             RadioButton radioButton = sender as RadioButton;
+            result.Text = "";
         }
 
         private void radioButton3_Unchecked(object sender, RoutedEventArgs e)
         {
-
+            RadioButton radioButton = sender as RadioButton;
+            result.Text = "";
         }
 
         private void checkBox1_Unchecked(object sender, RoutedEventArgs e)
